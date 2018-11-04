0b0912f16bfdfedcadd83493f514347c0c6efd25
t0shifollow <marcindymek24@gmail.com>
2018-11-04 15:57:17 +0100

    add logic lose/win

--- src/App.js
+++ src/App.js
@@ -9,18 +9,60 @@ import SubmitForm from "./components/game_view/SubmitForm";
 class App extends Component {
 
     state = {
-        word: words[Math.floor(Math.random() * words.length)], // randomowe losowanie liczb
-        guessedLetters: []
+        word: words[Math.floor(Math.random() * words.length)], // randomowe losowanie słów
+        guessedLetters: [],
+        guessesRemaining: 5
     }
+
+    updateGuessedLetters = (letter) => {
+        if (this.state.guessedLetters.includes(letter)) {
+            alert(`You already guessed ${letter}`)
+        } else {
+            this.setState({
+                guessedLetters: [...this.state.guessedLetters, letter]
+            })
+        }
+    }
+    
+    updateGuessesRemaining(letter) {
+        if (!this.state.guessedLetters.includes(letter) && !this.state.word.split('').includes(letter)){
+            this.setState({guessesRemaining: this.state.guessesRemaining -1})
+        }
+    }
+
+    gameOver(){
+        if(this.state.guessesRemaining <= 0) {
+            alert('You Have Lost')
+        } else if (this.wordIsGuessed()) {
+            alert(`YOU WON! Guessed word is: ${this.state.word}`)
+        }
+    }
+
+    wordIsGuessed(){
+        const guessState = this.state.word.split('').map(letter => {
+            if(this.state.guessedLetters.includes(letter)) {
+                return letter
+            }
+        })
+        return guessState.join('') === this.state.word
+    }
+
+    updateGameState(letter){
+        this.updateGuessedLetters(letter)
+        this.updateGuessesRemaining()
+        this.gameOver()
+    }
+
 
   render() {
     return (
       <div className="hangman">
         <h1>!HANGMAN GAME!</h1>
         <h2>let's start the game</h2>
+          <h3>{this.state.guessesRemaining}</h3>
           {this.state.word}
           <ShowWord word={this.state.word} guessedLetters={this.state.guessedLetters}/>
-          <SubmitForm />
+          <SubmitForm updateGameState={this.updateGameState} />
           <BadLetters word={this.state.word} guessedLetters={this.state.guessedLetters}/>
       </div>
     );
--- src/components/game_view/SubmitForm.js
+++ src/components/game_view/SubmitForm.js
@@ -2,10 +2,18 @@ import React, { Component } from "react";
 
 class SubmitForm extends Component {
 
+    _handleSubmit = (e) =>{
+       e.preventDefault()
+        const letter = this.refs.textInput.value
+        this.props.updateGameState(letter)
+        this.refs.textInput.value = ''
+
+    }
+
     render() {
         return (
             <div className="SubmitForm">
-                <form>
+                <form onSubmit={this._handleSubmit}>
                     <input type="text" ref='textInput' maxLength='1' />
                     <button>SUBMIT</button>
                 </form>
