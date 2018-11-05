4c7e6d581a373a2f746d059c93da8386ff495a68
t0shifollow <marcindymek24@gmail.com>
2018-11-05 01:18:02 +0100

    edit NotFound in components

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,9 +4,8 @@ const NotFound = () => (
     <div className="jumbotron">
         <div className="alert alert-dismissible alert-warning">
             <button type="button" className="close" data-dismiss="alert">&times;</button>
-            <h4 className="alert-heading">Warning!</h4>
-            <p className="mb-0">Best check yo self, you're not looking too good. Nulla vitae elit libero, a pharetra
-                augue. Praesent commodo cursus magna, <a href="/" className="alert-link">Come ON! Go back and play! :)</a>.</p>
+            <h4 className="alert-heading">NOT FOUND!</h4>
+            <p className="mb-0">UPPS!<a href="/" className="alert-link">Come ON! Go back and play! :)</a>.</p>
         </div>
     </div>
 );
